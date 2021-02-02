5c89039756f2496bf8e962e6b8e5e5f1b6e4f9c7
mosesmrima <mrimamss@gmail.com>
2021-02-02 23:57:49 +0300

    header file

---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/*
+ *header file containing all the function prototpyes
+ *of functions used in this prject
+ */
+#include <stdbool.h>
+#include "stdio.h"
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+#endif /* HEADER_FILE */

26dd504290917fb10776f7e06c3a34d6840c8d60
mosesmrima <mrimamss@gmail.com>
2021-10-25 19:22:54 +0300

    add insertion sort prototype

--- sort.h
+++ sort.h
@@ -8,5 +8,6 @@
 #include "stdio.h"
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 #endif /* HEADER_FILE */
